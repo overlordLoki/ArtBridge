2724b8c0fe41d5b6841d62128b14f3f6db10646b
overlordLoki <tymonisat@gmail.com>
2024-10-26 20:25:50 +1300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
-server_address = "127.0.0.1:8188"
+server_address = "192.168.1.88:8188"
 workflow_file_path = 'workflow/workflow_api.json'
 OUTPUTFOLDER = "/home/loki/projects/ai/art_ai/ComfyUI/output"
