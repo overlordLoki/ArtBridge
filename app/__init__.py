7f298fd34377ae27b664a2e4f648c64c232f1248
overlordLoki <tymonisat@gmail.com>
2024-10-27 00:20:53 +1300

    added wsgi for prod

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    
+    # Allow CORS for all origins
     CORS(app)
-
+    
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
